8493153a2d392fe8ca6f9f0b6ffd489dba13b812
Ryan <ryan@ryanandrew.dev>
2024-12-27 17:07:03 -0600

    build: update version naming

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,7 +25,7 @@ android {
         minSdk = 27
         targetSdk = 35
         versionCode = 1
-        versionName = "1.0"
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
